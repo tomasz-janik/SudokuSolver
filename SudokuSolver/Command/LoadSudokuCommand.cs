a4073c6713a479bbd4bac5f073f9f470f69fa63c
Tomasz Janik <tomasz.janik98@gmail.com>
2020-01-05 17:08:28 +0100

    Added sudoku loading from file

--- SudokuSolver/Command/LoadSudokuCommand.cs
+++ SudokuSolver/Command/LoadSudokuCommand.cs
@@ -1,10 +1,25 @@
-﻿namespace SudokuSolver.Command
+﻿using SudokuSolver.Parsing;
+using SudokuSolver.Reading;
+using SudokuSolver.Sudoku;
+
+namespace SudokuSolver.Command
 {
     public class LoadSudokuCommand : ICommand
     {
+        private readonly SudokuBoard _sudokuBoard;
+        private readonly Reader<string> _reader;
+        private readonly Parser<string> _parser;
+        
+        public LoadSudokuCommand(SudokuBoard sudokuBoard, Reader<string> reader, Parser<string> parser)
+        {
+            _sudokuBoard = sudokuBoard;
+            _reader = reader;
+            _parser = parser;
+        }
+
         public void Execute(string filename)
         {
-            throw new System.NotImplementedException();
+            _sudokuBoard.Cells = _parser.Parse(_reader.Read(filename));
         }
     }
 }
