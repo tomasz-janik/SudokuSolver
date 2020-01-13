968d5f3995ec544fcca8ac9ba6a6687bbc6599bc
Tomasz Janik <tomasz.janik98@gmail.com>
2020-01-13 20:02:14 +0100

    Testing Commands

---
+++ UnitTests/ViewModel/ClearSudokuCommandTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SudokuSolver.Model;
+using SudokuSolver.Model.Sudoku;
+using SudokuSolver.ViewModel.Command;
+
+namespace UnitTests.ViewModel
+{
+    public class ClearSudokuCommandTest
+    {
+        [TestCase(9, 9)]
+        [TestCase(0, 0)]
+        public void ExecuteTest(int rows, int columns)
+        {
+            var cells = new List<List<Cell>>(rows);
+            for (var i = 0; i < rows; i++)
+            {
+                var innerList = new List<Cell>(columns);
+                for (var j = 0; j < columns; j++)
+                {
+                    innerList.Add(new Cell(j + 1, State.SolverSet));
+                }
+
+                cells.Add(innerList);
+            }
+            var sudokuBoard = new SudokuBoard(new History()) {Cells = cells};
+            
+            var result = new ClearSudokuCommand(sudokuBoard).Execute(null);
+
+            foreach (var cell in sudokuBoard.Cells.SelectMany(lists => lists))
+            {
+                Assert.AreEqual(State.Unset, cell.State);
+                Assert.IsNull(cell.Value);
+            }
+            
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ UnitTests/ViewModel/LoadSudokuCommandTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using SudokuSolver.Model;
+using SudokuSolver.Model.Sudoku;
+using SudokuSolver.ViewModel.Command;
+using SudokuSolver.ViewModel.Provider;
+
+namespace UnitTests.ViewModel
+{
+    public class LoadSudokuCommandTest
+    {
+        [TestCase(9, 9, 2, true)]
+        [TestCase(0, 0, 2, false)]
+        public void ExecuteTest(int rows, int columns, int value, bool successful)
+        {
+            var cells = new List<List<Cell>>(rows);
+            for (var i = 0; i < rows; i++)
+            {
+                var innerList = new List<Cell>(columns);
+                for (var j = 0; j < columns; j++)
+                {
+                    innerList.Add(new Cell(value, State.InitialSet));
+                }
+
+                cells.Add(innerList);
+            }
+            var sudokuBoard = new SudokuBoard(new History()) {Cells = cells};
+            var fileProviderMock = new Mock<IFileProvider>();
+            fileProviderMock.Setup(e => e.Provide(It.IsAny<string>())).Returns(cells);
+            
+            var result = new LoadSudokuCommand(sudokuBoard, fileProviderMock.Object).Execute(null);
+
+            foreach (var cell in sudokuBoard.Cells.SelectMany(lists => lists))
+            {
+                Assert.AreEqual(State.InitialSet, cell.State);
+                Assert.AreEqual(value, cell.Value);
+            }
+            Assert.AreEqual(successful, result);
+        }
+    }
+}
