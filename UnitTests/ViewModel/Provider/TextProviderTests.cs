4145333249fbbabd3c1effcf8d2058ca5bfc1777
mastah7991 <mastah7991@gmail.com>
2020-01-13 21:10:31 +0100

    Add test testprovider

--- UnitTests/ViewModel/Provider/TextProviderTests.cs
+++ UnitTests/ViewModel/Provider/TextProviderTests.cs
@@ -15,7 +15,7 @@ namespace SudokuSolver.ViewModel.Provider.Tests
     public class TextProviderTests
     {
         [Test()]
-        public void ProvideTest()
+        public void ProvideValidatedTest()
         {
             var expectedResult = new List<List<Cell>>();
 
@@ -33,6 +33,28 @@ namespace SudokuSolver.ViewModel.Provider.Tests
             var textProvider = new TextProvider(textReader, textValidator);
 
             Assert.AreSame(expectedResult,textProvider.Provide("path"));
+
+        }
+
+        [Test()]
+        public void ProvideInValidatedTest()
+        {
+            var expectedResult = new List<List<Cell>>();
+
+            var validator = new Mock<IValidator>();
+            var parser = new Mock<IParser<string>>();
+            var reader = new Mock<IReader<string>>();
+
+            reader.Setup(x => x.Read(It.IsAny<string>())).Returns("test");
+            validator.Setup(x => x.Validate(It.IsAny<string>())).Returns(false);
+            parser.Setup(x => x.Parse(It.IsAny<string>())).Returns(expectedResult);
+
+            var textValidator = new TextParser(validator.Object, parser.Object);
+            var textReader = new TextReader(validator.Object, reader.Object);
+
+            var textProvider = new TextProvider(textReader, textValidator);
+
+            Assert.Null(textProvider.Provide("path"));
 
         }
     }
